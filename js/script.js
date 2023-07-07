932d71a5f32c5e47d4d24ff4088ce4b7c60a76af
Michaël <cornelismichael@hotmail.com>
2023-07-07 11:26:48 +0200

    first draft game logic

--- js/script.js
+++ js/script.js
@@ -21,14 +21,17 @@ const GAMEBOARD = (() => {
     const UPDATEBOARDVALUES = (sign, index) => {
         if(BOARDVALUES[index] === "") {
             BOARDVALUES[index] = sign;
+            UPDATEBOARD(sign, index);
             console.log(BOARDVALUES);
+            GAMECONTROLLER.CHECKWINNER(BOARDVALUES);
         } else {
             console.log("There is already a sign here!")
         }
     }
 //Responsible to visually update the board based on the array values
-    const UPDATEBOARD = () => {
-
+    const UPDATEBOARD = (sign, index) => {
+        const CELL = document.querySelector(`[data-index="${index}"]`);
+        CELL.textContent = sign;
     }
 
     return {RENDERBOARD, UPDATEBOARDVALUES, UPDATEBOARD};
@@ -48,42 +51,67 @@ const GAMECONTROLLER = (() => {
     const PLAYERONE = PLAYER("Michaël", 'X');
     const PLAYERTWO = PLAYER("God", 'O');
     let currentPlayer = PLAYERONE;
+    let currentPlayerName = currentPlayer.GETNAME();
+    let currentPlayerSign = currentPlayer.GETSIGN();
+    let playing = true;
 
     const WINNINGCOMBINATIONS = [
         [0,1,2],[3,4,5],[6,7,8],
         [0,3,6],[1,4,7],[2,5,8],
         [0,4,8],[6,4,2]
     ]
-
-    GAMEBOARD.RENDERBOARD();
-
-
-    //2 dingen moeten gebeuren bij klik: sign van huidige player moet naar Array gepushed worden
-    //Mogelijks functie updateBOARD bij GAMEBOARD toevoegen
-    
-    const SETUPEVENTLISTENERS = () => {
-        const CELLS = document.querySelectorAll('.cell');
-        CELLS.forEach(cell => {
-            cell.addEventListener('click', () => {
+   
+   const SETUPEVENTLISTENERS = () => {
+    const CELLS = document.querySelectorAll('.cell');
+    CELLS.forEach(cell => {
+        cell.addEventListener('click', () => {
+            if(playing) {
                 const CURRENTVALUE = cell.getAttribute('value');
                 if(CURRENTVALUE !== null) {
                     console.log("Cell already occupied. Pick another.")
                 } else {
-                    const PLAYERSIGN = currentPlayer.GETSIGN();
-                    cell.textContent = PLAYERSIGN;
-                    cell.setAttribute('value', PLAYERSIGN);
+                    console.log("Active player: " + currentPlayerName);
+                    cell.setAttribute('value', currentPlayerSign);
                     //update the array of the board
                     const INDEX = cell.getAttribute('data-index');
-                    GAMEBOARD.UPDATEBOARDVALUES(PLAYERSIGN,INDEX);
+                    GAMEBOARD.UPDATEBOARDVALUES(currentPlayerSign,INDEX);
+                    SWITCHPLAYER();
                 }
-            })
+            }
+        })
+    })
+}
+
+    const SWITCHPLAYER = () => {
+        if(currentPlayer === PLAYERONE) {
+            currentPlayer = PLAYERTWO;
+            currentPlayerName = currentPlayer.GETNAME();
+            currentPlayerSign = currentPlayer.GETSIGN();
+        } else {
+            currentPlayer = PLAYERONE;
+            currentPlayerName = currentPlayer.GETNAME();
+            currentPlayerSign = currentPlayer.GETSIGN();
+        }
+    }
+
+    const CHECKWINNER = (BOARDVALUES) => {
+        WINNINGCOMBINATIONS.forEach(combination => {
+            const [a,b,c] = combination;
+            if(
+                BOARDVALUES[a] === currentPlayerSign &&
+                BOARDVALUES[b] === currentPlayerSign &&
+                BOARDVALUES[c] === currentPlayerSign
+            ) {
+                console.log(currentPlayerName + " wins the game!")
+                playing = false;
+            }
         })
     }
 
-
+    GAMEBOARD.RENDERBOARD();
     SETUPEVENTLISTENERS();
 
-    return {}
+    return {CHECKWINNER}
 
 })();
 
